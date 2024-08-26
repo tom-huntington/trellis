4896e022560a7d35d781f7abe66b1cb31bc52082
tom-huntington <55266932+tom-huntington@users.noreply.github.com>
2024-08-26 20:01:51 +1200

    add imports

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from lark import Lark, Transformer
 from trellis_builtins import *
 import io
-from re import findall
 import functools
-from operator import add
 
-ex = "map { findall { r'(\d)' } | first add last . phi | int } | sum"
+ex = """\
+from re import findall
+from operator import add
+map { findall { r'(\d)' } | first add last . phi | int } | sum
+"""
 
 grammar = """\
 func: NON_WHITESPACE
@@ -18,14 +20,15 @@ call_nt: call_nt? arg* "." func trailing1 -> call
 call_t: call_nt? arg* func "\\\\" trailing2 -> call
 call_p: call_nt? arg* "|" _trailing3
 ?call: call_nt | call_t | call_p
-?start: call | partial
+import: "from" NAME "import" NAME "\\n"
+start: import* (call | partial)
 
 NON_WHITESPACE: /(?!\\\\)[^\s{}.|]+/
 
 %import common.CNAME -> NAME
-%import common.WS_INLINE
+%import common.WS
 %import common.SIGNED_NUMBER
-%ignore WS_INLINE
+%ignore WS
 %import common.ESCAPED_STRING
 """
 
@@ -59,8 +62,22 @@ class Parser(Transformer):
     def call_p(self, args):
         r = b(*args)
         return r
+    
+    # def start(self, args):
+    #     for tree in args[:-1]:
+    #         package, a = tree.children
+    #         exec(f"from {package} import {a}")
+        
+        return args[-1]
+
+*imports, ast = ast.children
+
+for tree in imports:
+        package, a = tree.children
+        exec(f"from {package} import {a}")
 
 program = Parser().transform(ast)
+
 print(program(io.StringIO("""1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
